9ac2c4f55a974e17441a5b376b1cc0b5132b0570
Septdir <septdir@gmail.com>
2018-03-07 16:05:49 +0300

    Git fix

--- plg_fieldtypes_advtags/fields/advtags.php
+++ plg_fieldtypes_advtags/fields/advtags.php
@@ -312,7 +312,7 @@ class JFormFieldAdvTags extends FormField
 			->join('INNER', '#__tags as this ON t.lft > this.lft AND t.rgt < this.rgt')
 			->where('this.id IN (' . $this->parents . ')');
 
-		// ublished and Access
+		// Published and Access
 		if ($app->isSite())
 		{
 			$query->where($db->quoteName('t.published') . ' = ' . 1)

51664d73d827446f466754b55376c115dda5ed40
Septdir <septdir@gmail.com>
2018-05-18 14:09:32 +0300

    Change version to 1.0.5

--- plg_fieldtypes_phones/media/field.js
+++ plg_fieldtypes_phones/media/field.js
@@ -1,6 +1,6 @@
 /*
  * @package    Field Types - Phones Plugin
- * @version    1.0.4
+ * @version    1.0.5
  * @author     Nerudas  - nerudas.ru
  * @copyright  Copyright (c) 2013 - 2018 Nerudas. All rights reserved.
  * @license    GNU/GPL license: http://www.gnu.org/copyleft/gpl.html
--- plg_fieldtypes_price/price.php
+++ plg_fieldtypes_price/price.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @package    Field Types - Price Plugin
- * @version    1.0.4
+ * @version    1.0.5
  * @author     Nerudas  - nerudas.ru
  * @copyright  Copyright (c) 2013 - 2018 Nerudas. All rights reserved.
  * @license    GNU/GPL license: http://www.gnu.org/copyleft/gpl.html

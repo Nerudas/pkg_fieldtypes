020df63e369988837eed5cdff25e769db67ad9f6
Septdir <septdir@gmail.com>
2018-06-14 15:58:58 +0300

    Add exits check

--- plg_fieldtypes_ajaxalias/media/css/ajaxalias.css
+++ plg_fieldtypes_ajaxalias/media/css/ajaxalias.css
@@ -1,6 +1,6 @@
 /*
  * @package    Field Types - Ajax Alias Plugin
- * @version    1.0.5
+ * @version    1.0.6
  * @author     Nerudas  - nerudas.ru
  * @copyright  Copyright (c) 2013 - 2018 Nerudas. All rights reserved.
  * @license    GNU/GPL license: http://www.gnu.org/copyleft/gpl.html

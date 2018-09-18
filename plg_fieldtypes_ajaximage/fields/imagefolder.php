eb121ac5b1a6b190024a66ba1c470acfbb1ba441
Septdir <septdir@gmail.com>
2018-09-18 12:42:14 +0300

    Change version to 1.1.1

--- plg_fieldtypes_ajaximage/fields/imagefolder.php
+++ plg_fieldtypes_ajaximage/fields/imagefolder.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @package    Field Types - Ajax Image Plugin
- * @version    1.1.0
+ * @version    1.1.1
  * @author     Nerudas  - nerudas.ru
  * @copyright  Copyright (c) 2013 - 2018 Nerudas. All rights reserved.
  * @license    GNU/GPL license: http://www.gnu.org/copyleft/gpl.html
--- plg_fieldtypes_files/media/css/images.css
+++ plg_fieldtypes_files/media/css/images.css
@@ -1,6 +1,6 @@
 /*
  * @package    Field Types - Files Plugin
- * @version    1.1.0
+ * @version    1.1.1
  * @author     Nerudas  - nerudas.ru
  * @copyright  Copyright (c) 2013 - 2018 Nerudas. All rights reserved.
  * @license    GNU/GPL license: http://www.gnu.org/copyleft/gpl.html

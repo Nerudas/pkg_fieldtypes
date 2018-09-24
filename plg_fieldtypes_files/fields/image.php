3b0f8040d7d56e17931f01e3fb64916a638c2f3a
Septdir <septdir@gmail.com>
2018-09-24 13:10:04 +0300

    Add trim

--- plg_fieldtypes_files/fields/image.php
+++ plg_fieldtypes_files/fields/image.php
@@ -119,7 +119,7 @@ class JFormFieldImage extends FormField
 			$this->form->getField($this->folder_field)->id;
 		$params['noimage']      = (!empty($this->noimage)) ? $this->noimage : '';
 		$params['filename']     = $this->filename;
-		$params['site_root']    = Uri::root(true) . '/';
+		$params['site_root']    = trim(Uri::root(true), '/') . '/';
 
 		Factory::getDocument()->addScriptOptions($this->id, $params);
 

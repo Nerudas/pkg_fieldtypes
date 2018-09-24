f7e8b1b0fcc62eb5af53c3dcbb29b2e5a1941557
Septdir <septdir@gmail.com>
2018-09-24 12:36:05 +0300

    Change version to 1.1.3

--- plg_fieldtypes_ajaxalias/media/css/ajaxalias.css
+++ plg_fieldtypes_ajaxalias/media/css/ajaxalias.css
@@ -1,6 +1,6 @@
 /*
  * @package    Field Types - Ajax Alias Plugin
- * @version    1.1.2
+ * @version    1.1.3
  * @author     Nerudas  - nerudas.ru
  * @copyright  Copyright (c) 2013 - 2018 Nerudas. All rights reserved.
  * @license    GNU/GPL license: http://www.gnu.org/copyleft/gpl.html

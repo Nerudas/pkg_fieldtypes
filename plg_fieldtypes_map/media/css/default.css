88cf2ca48cdd5b5d532308a6c588525b14192e7f
Septdir <septdir@gmail.com>
2018-11-09 15:18:46 +0300

    Fix folder field

--- plg_fieldtypes_map/media/css/default.css
+++ plg_fieldtypes_map/media/css/default.css
@@ -1,6 +1,6 @@
 /*
  * @package    Field Types - Map Plugin
- * @version    1.1.5
+ * @version    1.1.6
  * @author     Nerudas  - nerudas.ru
  * @copyright  Copyright (c) 2013 - 2018 Nerudas. All rights reserved.
  * @license    GNU/GPL license: http://www.gnu.org/copyleft/gpl.html
